8e586d82d33c953c8faaf817c41160de62f33349
eaudejavel <paul.lamy@hetic.net>
2022-12-09 18:18:23 +0100

    test feature init

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -13,57 +13,122 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  // Declare an AnimationController
+  late AnimationController _controller;
+  // Declare an Animation for the size of the HeartWidget
+  late Animation<double> _sizeAnimation;
+  // Declare an Animation for the color of the HeartWidget
+  late Animation<Color?> _colorAnimation;
   List<String>? sentences;
 
   @override
   void initState() {
     super.initState();
+
+    // Initialize the AnimationController
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+      debugLabel: 'HeartAnimation',
+    );
+
+    // Initialize the _sizeAnimation field
+    _sizeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+
+    // Use the Tween to animate the size of the HeartWidget
+    _sizeAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(_sizeAnimation);
+
+    // Create a ColorTween to animate the color of the HeartWidget
+    ColorTween colorTween = ColorTween(
+      begin: Colors.green,
+      end: Colors.red,
+    );
+
+    // Create an Animation for the color of the HeartWidget
+    _colorAnimation = colorTween.animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    ));
+
     // Load the sentences when the widget is initialized
     loadSentences();
   }
 
   @override
+  void dispose() {
+    // Dispose of the AnimationController when the widget is disposed
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: FutureBuilder(
-        future: loadSentences(), // Call the loadSentences function
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, check if it was successful or not
-            if (snapshot.hasError) {
-              return Center(
-                child: Text("An error occurred: ${snapshot.error}"),
-              );
-            } else {
-              // The Future was successful, so we can extract the sentences from the snapshot
-              // Check if the sentences list is null
-              if (sentences == null) {
-                return const Center(
-                  child: Text("Sorry, no sentence is available for today."),
-                );
-              } else {
-                // The sentences list is not null, so we can use it
-                // Calculate the index of the sentence to display
-                int index = DateTime.now().day % sentences!.length;
-                print(index);
-                return Center(
-                  child: Text(sentences![index]),
-                );
-              }
-            }
-          } else {
-            // The Future is still pending, so show a loading indicator
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          // Trigger the heart animation when the button is clicked
+          onPressed: () => _controller.forward(),
+          child: const Icon(Icons.favorite),
+        ),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FutureBuilder(
+              future: loadSentences(), // Call the loadSentences function
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the Future is complete, check if it was successful or not
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text("An error occurred: ${snapshot.error}"),
+                    );
+                  } else {
+                    // The Future was successful, so we can extract the sentences from the snapshot
+                    // Check if the sentences list is null
+                    if (sentences == null) {
+                      return const Center(
+                        child:
+                            Text("Sorry, no sentence is available for today."),
+                      );
+                    } else {
+                      // The sentences list is not null, so we can use it
+                      // Calculate the index of the sentence to display
+                      int index = DateTime.now().day % sentences!.length;
+                      print(index);
+                      return Center(
+                        child: Text(sentences![index]),
+                      );
+                    }
+                  }
+                } else {
+                  // The Future is still pending, so show a loading indicator
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+            Center(
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) => HeartWidget(
+                  size: _sizeAnimation.value,
+                  color: _colorAnimation.value,
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 
   Future<List<String>?> loadSentences() async {
@@ -88,5 +153,25 @@ class _MyHomePageState extends State<MyHomePage> {
 
     // Return the sentences list
     return sentences;
+  }
+}
+
+class HeartWidget extends StatelessWidget {
+  const HeartWidget({
+    super.key,
+    required this.size,
+    required this.color,
+  });
+
+  final double size;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      Icons.favorite,
+      size: size,
+      color: color,
+    );
   }
 }
