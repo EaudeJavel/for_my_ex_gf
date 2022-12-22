62289b909816b092db72f8f855156749bb80a182
eaudejavel <paul.lamy@hetic.net>
2022-12-22 21:09:57 +0100

    feature: upgrade to periodical notificiation

--- lib/utils/notif_service.dart
+++ lib/utils/notif_service.dart
@@ -1,13 +1,17 @@
-// ignore_for_file: unnecessary_null_comparison, import_of_legacy_library_into_null_safe, unused_import, unnecessary_import,, prefer_equal_for_default_values
+// ignore_for_file: unnecessary_null_comparison, import_of_legacy_library_into_null_safe, unused_import, unnecessary_import,, prefer_equal_for_default_values, depend_on_referenced_packages
 import 'dart:ui';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:no_more_anxiety/routes.dart';
 
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
 class NotificationService {
   static final NotificationService _notificationService =
       NotificationService._internal();
+
   factory NotificationService() {
     return _notificationService;
   }
@@ -17,6 +21,8 @@ class NotificationService {
       FlutterLocalNotificationsPlugin();
 
   Future<void> initNotifs() async {
+    tz.initializeTimeZones();
+
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/ic_launcher');
 
@@ -34,41 +40,45 @@ class NotificationService {
 
     await flutterLocalNotificationsPlugin.initialize(initializationSettings,
         onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
+
+    //Set default location to Paris
+    tz.setLocalLocation(tz.getLocation('Europe/Paris'));
   }
 
+  @pragma('vm:entry-point')
   Future<void> showNotification() async {
-    await flutterLocalNotificationsPlugin.show(
-      0,
-      'Notif title',
-      'Notif body',
-      platformChannelSpecifics,
-      payload: 'item x',
-    );
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        'scheduled title',
+        'scheduled body',
+        tz.TZDateTime.now(tz.local).add(const Duration(seconds: 10)),
+        const NotificationDetails(
+            android: AndroidNotificationDetails('1', 'random channel',
+                importance: Importance.max,
+                priority: Priority.high,
+                ticker: 'ticker')),
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime);
   }
 
   void onDidReceiveNotificationResponse(
       NotificationResponse notificationResponse) async {
     final String? payload = notificationResponse.payload;
-    var navigatorKey = GlobalKey<NavigatorState>();
-    final BuildContext context = navigatorKey.currentContext!;
     if (notificationResponse.payload != null) {
       debugPrint('notification payload: $payload');
     }
-    await Navigator.push(
-      context,
-      MaterialPageRoute<void>(builder: (context) => const MyHomePage()),
-    );
   }
 }
 
-const AndroidNotificationDetails androidPlatformChannelSpecifics =
-    AndroidNotificationDetails(
-  '1',
-  'random channel',
-  importance: Importance.max,
-  priority: Priority.high,
-  ticker: 'ticker',
-);
+// const AndroidNotificationDetails androidPlatformChannelSpecifics =
+//     AndroidNotificationDetails(
+//   '1',
+//   'random channel',
+//   importance: Importance.max,
+//   priority: Priority.high,
+//   ticker: 'ticker',
+// );
 
-const NotificationDetails platformChannelSpecifics =
-    NotificationDetails(android: androidPlatformChannelSpecifics);
+// const NotificationDetails platformChannelSpecifics =
+//     NotificationDetails(android: androidPlatformChannelSpecifics);
