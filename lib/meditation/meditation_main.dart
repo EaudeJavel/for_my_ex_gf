e1d485e75f358522ca84a99bdcb6dc06fe7ddb36
EaudeJavel <p.plamy6@gmail.com>
2022-12-27 22:34:02 +0100

    fix: merge

--- lib/meditation/meditation_main.dart
+++ lib/meditation/meditation_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 class MeditationScreen extends StatefulWidget {
   const MeditationScreen({super.key});
@@ -13,11 +14,33 @@ class _MeditationScreen extends State<MeditationScreen> {
     super.initState();
   }
 
+  void createWave() {
+    for (int i = 0; i <= CircleAvatar.radius; i++) {
+      sinWave.add(new Offset(i.toDouble(),
+      sin((time.value * 360 - i) % 360 * degrees2Radians) * 20));
+}
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mon espace méditation"),
+        centerTitle: true,
+      ),
       body: Center(
-        child: Text("jtm"),
+        child: GestureDetector(
+          child: const CircleAvatar(
+            radius: 100,
+            backgroundColor: Colors.pink,
+            child: Text("Méditer"),
+          ),
+          onTap: () {
+            if (kDebugMode) {
+              print("object");
+            }
+          },
+        ),
       ),
     );
   }
